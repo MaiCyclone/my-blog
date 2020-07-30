71b26ccedfaaca2396fdf513716ec1f049e0f02b
Jay <yuxuanjiang@JJJs-MacBook-Pro.local>
2020-07-30 13:31:01 -0400

    first committttttttt

---
+++ src/build/precache-manifest.630bab601b900121abe5a197af98ffe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0785431a30a4e6f18bef77f809d7d4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e8eb8ba6bb534b054a",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "692dbdaf31b91529bf45",
+    "url": "/static/js/2.f541dabc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f541dabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e8eb8ba6bb534b054a",
+    "url": "/static/js/main.6473db5d.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
